0f058473282921e1aaae44e0d3f144eb063f44ee
babajix-lol <raziwolesdi@gmail.com>
2025-07-31 15:18:41 +0700

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int time = 22;
+    if(time < 10) {
+        cout << "good morning";
+    } else if (time < 20) {
+        cout<< "good day.";
+    }else{
+        cout<<"good evening.";
+    }
+    return 0;
+}
+    
