7f8ec670664286408201f9249878f55a052d5324
babajix <raziwolesdi@gmail.com>
2025-11-06 14:20:11 +0700

    terserah
    ga ush

---
+++ skrip.js
@@ -0,0 +1,20 @@
+const learnBtn = document.querySelector('.btn[href="#testimoni"]');
+
+if (learnBtn){
+    learnBtn.addEventListener("click", e => {
+        e.preventDefault();
+        const target = document.querySelector("#testimoni");
+        if (target){
+            target.scrollIntoView({behavior: 'smooth'});
+        }
+    })
+}
+
+const form = document.querySelector('.contact-form');
+if (form){
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        alert("Form submitted!");
+        form.reset();
+    })
+}
